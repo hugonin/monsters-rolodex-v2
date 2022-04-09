fd1512b439d7b5223c4558284ae0362046897970
Hugonin <jeanhugues.doumbe@gmail.com>
2022-04-09 16:51:31 +0200

    feat: Added a function in Create page  to handle the change of monster images

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -58,6 +58,8 @@ export default function Create() {
 
  }
 
+ const url = "https://robohash.org/" + handleImageChange(20) + "?set=set2"
+
 
   return (
     <div className="container mx-auto px-8 my-12">
@@ -132,7 +134,7 @@ export default function Create() {
                         </span>
                         <button
                           onClick={(e) => setImage(e.target.value)}
-                          value= "https://robohash.org/image.id?set=set2"
+                          value= {url}
                           type="button"
                           className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
